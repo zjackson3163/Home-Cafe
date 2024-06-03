29bf0e8484174f8c080fbe62c537c172bfaef4ef
zjackson3163 <93844448+zjackson3163@users.noreply.github.com>
2024-06-03 14:57:10 -0400

    incorporating mvc

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name = "description" content = "Recipes for a cafe experience at home!">
-        <meta name = "keywords" content = "Home Cafe, cafe, cafe recipes, coffee recipes, bread recipes, cake recipes, at home cafe">
-        <meta name = "name" content = "Zorayah Jackson">
-        <link rel = "icon" href = "images/favicon1.png">
-        <title>Home Café || Home</title>
-        <link href='https://fonts.googleapis.com/css?family=Sacramento' rel='stylesheet'>
-        <link href="https://fonts.googleapis.com/css2?family=Outfit&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Rowdies:wght@300;400;700&family=Zilla+Slab:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
-        <link href = "styles/style.css" rel = "stylesheet"/>
+    <?php include("header.html") ?>
+    <title>Home Café || Home</title>
     </head>
 
     <body class = "main">
